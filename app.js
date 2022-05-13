e6aa75b2db1740ea7a73a33cb0390954f7b5fca5
Merano Tu <merano5342@gmail.com>
2022-05-13 16:44:58 +0800

    make restaurant search in en mane available

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.get('/restaurants/:restaurants_id', (req, res) => {
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword) || restaurant.location.includes(keyword)
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.includes(keyword) || restaurant.location.includes(keyword)
   })
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
